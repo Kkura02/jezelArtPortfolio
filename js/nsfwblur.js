7daa59562c24160435bca72906195baa1bb7d170
Geno <57450665+Kkura02@users.noreply.github.com>
2022-11-22 00:22:21 -0800

    nsfw blur button fixed

--- js/nsfwblur.js
+++ js/nsfwblur.js
@@ -2,14 +2,17 @@ function myFunction() {
     // Get the checkbox
     var checkBox = document.getElementById("myCheck");
 
-    var img = document.getElementsByClassName("nsfwimg");
   
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true){
         console.log("Hello world!");
-        document.getElementsByClassName("img").style.filter = "blur(0px)";
+        document.getElementById("nsfwimg1").style.filter = "blur(0px)";
+        document.getElementById("nsfwimg2").style.filter = "blur(0px)";
+        document.getElementById("nsfwimg3").style.filter = "blur(0px)";
     } else {
         console.log("hatsus not");
-        document.getElementsByClassName("img").style.filter = "blur(25px)";
+        document.getElementById("nsfwimg1").style.filter = "blur(25px)";
+        document.getElementById("nsfwimg2").style.filter = "blur(25px)";
+        document.getElementById("nsfwimg3").style.filter = "blur(25px)";
     }
   }
